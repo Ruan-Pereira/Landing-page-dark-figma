7429007d332e637b64a435eac1c6c836a1bd60d8
Ruan Pereira <ruanborbapereira@gmail.com>
2021-02-13 14:24:48 -0300

    finalizando a "feature" de acordo com o layout

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
       <div>
         <Header/>
         <Banner/>
-        {/* <Feature/>
-        <Contents/>
+        <Feature/>
+        {/* <Contents/>
         <Galeria/>
         <Patners/>
         <Comentario/>
--- src/pages/Home/components/feature/index.js
+++ src/pages/Home/components/feature/index.js
@@ -1,10 +1,78 @@
 import React  from "react";
 import "./style.css"
+import Food from "../../../../images/ic_round-restaurant-menu.svg"
+import Alien from "../../../../images/alien.svg"
+import Infiniti from "../../../../images/infiniti.svg"
+import ChatBot from "../../../../images/ChatBot.svg"
+import LineChart from "../../../../images/LineChart.svg"
+
+
+
 
 export default  () => {
   return (
-   <div className="teste">
-     olá mundo contents
+   <div className="feature">
+     <div className="container">
+       
+        <div className="container-text">
+          <h2>Features</h2>
+          <p>Most calendars are designed for teams. 
+          Slate is designed for freelancers who want 
+          a simple way to plan their schedule.</p>
+        </div>
+
+        <div className="container-image">
+              <img  className="img-first" src={ChatBot} alt=""/>
+              <img  className="img-second" src={LineChart} alt=""/>
+        </div>
+
+        <div className="container-functions">
+
+            <div className="container-functions-text">
+                <div className="container-functions-text-header">
+                    <img src={Food} alt=""/>
+                    A single source of truth
+                </div>
+
+                <div className="container-functions-text-footer">
+                    <p>When you add work to your 
+                    Slate calendar we automatically 
+                    calculate useful insights.</p>
+                </div>
+            </div>
+
+            
+            <div className="container-functions-text">
+                <div className="container-functions-text-header">
+                    <img src={Alien} alt=""/>
+                    Intuitive interface
+                </div>
+
+                <div className="container-functions-text-footer">
+                    <p>When you add work to your 
+                    Slate calendar we automatically 
+                    calculate useful insights.</p>
+                </div>
+            </div>
+
+            
+            <div className="container-functions-text">
+                <div className="container-functions-text-header">
+                    <img src={Infiniti} alt=""/>
+                    Or with rules
+                </div>
+
+                <div className="container-functions-text-footer">
+                    <p>When you add work to your 
+                    Slate calendar we automatically 
+                    calculate useful insights.</p>
+                </div>
+            </div>
+            
+            
+        </div>
+
+        </div>
    </div>
   );
 }
